bd6db45d81a442be1177a335063f96cf8a745755
AKChizhov <ChizhovAK2007@yandex.ru>
2022-11-27 20:10:13 +0300

    Длинный

--- HomeWork№02/toSeminar02-04.py
+++ HomeWork№02/toSeminar02-04.py
@@ -2,25 +2,53 @@
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 # Для продвинутых - с файлом ( вариант минимум - ввести позиции в консоли) -2 -1 0 1 2 Позиции: 0,1 -> 2
 import os
-number = int(input('Введите число N : ') )
-#numberMap = list(map(int ,range( -number,  (number + 1))))
-numberMap = range( -number,  (number + 1))
-numberList = []
-for i in numberMap:
-    numberList.append(int( numberMap[i] ))
-numberList.sort()
-try:
-    somefile = open("dataTo04.txt", "w")
-    try:
-        #somefile.write("hello Sasha !")
-        with open('dataTo04.txt', 'a') as file:
-            print(numberList, file=file )  
-        somefile.write("hello Sasha !")     
-    except Exception as e:
-        print(e)
-    finally:
-        somefile.close()
-except Exception as ex:
-    print(ex)
+os.system('CLS')
 
-os.system('type dataTo04.txt')
+FILENAME = "messages.txt"#Название файла
+messages = list()#Сщоздан пустой список
+ 
+number = int(input('Введите число N : ') ) 
+for i in range(-number,  (number + 1)):
+    message = str(int(i))
+    messages.append(message + '\n')
+
+# запись списка в файл
+with open(FILENAME, "w") as file:
+    for message in messages:
+        file.write(message)
+print(message, end='')
+print(f'Список из элементов, заполненных числами из промежутка от : {-number} по: {number} записан в файл {FILENAME}')
+ 
+# считываем сообщения из файла
+dat = []
+with open(FILENAME, "r") as file:
+    i=0
+    for message in file:
+        dat.append(int(message))
+print(dat)
+print(f'Список из элементов, заполненных числами из промежутка от : {-number} по: {number} считан из  файла {FILENAME}')
+
+mult = 1
+i = 0
+while (i < len(dat)):
+    if(dat[i] != 0):
+        mult = mult * dat[i]
+    i+= 1
+print(f'\nПроизведение чисел от : {-number}  по: {number} ( 0 не учитывается ) равно : {mult}')
+
+print(f'Введите через пробелы или запятую  2 числа из выше указанного интервала от  {-number}  по: {number}')
+borders = input()
+borders = borders.replace(',', '')
+borders = list(map(int,borders.split()))
+i = 0
+while(i < len(dat)):
+    if(dat[i] == borders[0]): iStart = i 
+    if(dat[i] == borders[1]): iEnd = i
+    i+=1     
+mult = 1
+i = iStart
+while (i  <= iEnd):
+    if(dat[i] != 0):
+        mult = mult * dat[i]
+    i+= 1
+print(f'\nПроизведение чисел от : {borders[0]}  по: {borders[1]} ( 0 не учитывается ) равно : {mult}')
