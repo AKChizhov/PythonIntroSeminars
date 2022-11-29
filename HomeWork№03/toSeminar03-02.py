93c45d41f45ab60bfe0763f9045cf99775540d05
AKChizhov <ChizhovAK2007@yandex.ru>
2022-11-29 11:39:16 +0300

    Добавлено ДЗ №2 к семинару №3

--- HomeWork№03/toSeminar03-02.py
+++ HomeWork№03/toSeminar03-02.py
@@ -9,18 +9,20 @@ import math
 
 def main():
     os.system('CLS')
+    print_message()
     volume = create_volume()
     list1 = fill_list (volume)
-    number_list_mult (list1)
-    list_pairs (list1)
     list_and_mult_pairs(list1)
     
+def print_message():
+    print('\nБудет сгенерирован список из чисел от 1 по 10. Кол-во чисел от 3 по 9 - для наглядности.')    
+    
 def create_volume() -> int:
-    volume = random.randint(3,9)
+    volume = random.randint(3,9)#Определяем кол-во элементов списка (для наглядности  от 3 по 9)
     return volume
 
 def fill_list(volume : int ) -> int:
-    listOfNumbers = []
+    listOfNumbers = []#Заполняем с помощью генерации исходный список (для наглядности числами от 1 по 10).
     i = 0
     while( i < volume):
         temp = random.randint(1,10)
@@ -29,33 +31,15 @@ def fill_list(volume : int ) -> int:
     print(f'\nИсходный сгенерированный список : {listOfNumbers}')
     return listOfNumbers
     
-def number_list_mult(list1 : list) -> int:
-    list2 = []
-    i = 0
-    while(i < (len(list1) / 2)):
-        list2.append(list1[i] * list1[len(list1) - 1 - i])
-        i+=1
-    print(f'\nСписок из произведение пар крайних чисел исходного списка : {list2}')
-    return list2
-
-def list_pairs(list1 : list) -> str:
-    list3 = []
+def list_and_mult_pairs (list1 : int):
+    list2 = []#Список из произведение пар крайних чисел исходного списка
+    list3 = []#Список из пар крайних чисел исходного списка
     i = 0
     while(i < (len(list1) / 2)):
         list3.append(f'{list1[i]} * {list1[len(list1) - 1 - i]}')
+        list2.append(list1[i] * list1[len(list1) - 1 - i])
         i+=1
     print(f'\nСписок из пар крайних чисел исходного списка : {list3}\n')
-    return list3
-
-def list_and_mult_pairs (list1 : int):
-    list2 = []
-    list3 = []
-    i = 0
-    while(i < (len(list1) / 2)):
-        list3.append(f'{list1[i]} * {list1[len(list1) - 1 - i]}')
-        list2.append(list1[i] * list1[len(list1) - 1 - i])
-        i+=1
-    print(f'\n\nСписок из пар крайних чисел исходного списка : {list3}\n')
     print(f'Список из произведение пар крайних чисел исходного списка : {list2}\n')
         
 main()# Вызов функции main
