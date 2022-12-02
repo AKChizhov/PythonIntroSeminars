47fd2546eacd86641e8592a4d2bf63b9a364bba8
AKChizhov <ChizhovAK2007@yandex.ru>
2022-12-02 18:47:44 +0300

    Доработаны функции в  ДЗ №2 к семинару №4

--- HomeWork№04/toSeminar04-02.py
+++ HomeWork№04/toSeminar04-02.py
@@ -3,26 +3,30 @@
 # 144 -> [2,3]
 
 import os
+import time
 
 def main():
     os.system('CLS')
-    my_number = enter_number()
-    my_list_of_factors = list_of_factors(my_number)
+    my_number = enter_number()#Вводимое число
+    time.sleep(4)
+    os.system('CLS')  
+    my_list_of_factors = list_of_factors(my_number)#Список всех множителей числа N
+    list_of_simpl_factors(my_number, my_list_of_factors)#Вывод на экран списка простых множителей числа N
     
-def enter_number()  -> int:
-    number = int(input('Введите число :'))   
-    print(f'Введено число : {number}') 
+def enter_number()  -> int:#Ввод числа
+    number = int(input('Введите число : '))   
+    print(f'Введено число : {number} Думаю....') 
     return number
 
-def list_of_factors(number : int) -> list[int]:
+def list_of_factors(number : int) -> list[int]: #Составление списка всех множителей числа N
     listing = []
     for i in range(1,number + 1):
         temp = number % i
-        if(temp == 0 ): listing.append(i)   
-    #print(listing)  
+        if(temp == 0 ): listing.append(i)  
+    return listing
 
-    listing_result = [1] 
-    
+def list_of_simpl_factors(number : int , listing : list[int]):#Составление списка простых множителей из списка всех множителей числа N
+    listing_result = [1]    
     i =  1
     while(i < len(listing)):
         j = 1
@@ -36,6 +40,6 @@ def list_of_factors(number : int) -> list[int]:
             j+=1
         if(my_flag == 1): listing_result.append(listing[i])
         i+=1
-    print(f'Список простых множителей введенного числа {number} : {listing_result}\n')
+    print(f'\nСписок простых множителей введенного числа {number} : {listing_result}\n')
      
 main()
