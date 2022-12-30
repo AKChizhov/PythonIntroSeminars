b69ee3cfc8f1e9d44ac020c859224259d755f5f9
AKChizhov <ChizhovAK2007@yandex.ru>
2022-12-31 01:53:24 +0300

    Доработано ДЗ к семинару №10

--- HomeWork№10/req.py
+++ HomeWork№10/req.py
@@ -1,9 +1,12 @@
 import requests, json
+from datetime import date
 
 #res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
 
 result = json.loads(res.text)
-
+today = date.today()
+temp2 = "{}.{}.{}".format(today.day, today.month, today.year)     
+print(f' На {temp2} курс валют :')  
 print(result['Valute']["USD"]['Name'], result['Valute']["USD"]['Value'])
 print(result['Valute']["EUR"]['Name'], result['Valute']["EUR"]['Value'])
--- HomeWork№10/toSeminar10_02.py
+++ HomeWork№10/toSeminar10_02.py
@@ -1,4 +1,5 @@
 import telebot, config, os, json, requests
+from datetime import date
 bot = telebot.TeleBot(config.TOKEN)
 
 os.system('CLS')
@@ -6,14 +7,17 @@ def find(s):
     try:
         res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         result = json.loads(res.text)  
-        #out = (result['Valute']["USD"]['Name'] , result['Valute']["USD"]['Value'])  
-        out = ( result['Valute'][s]['Value'])  
+        s = s.upper()
+        temp1 = str (result['Valute'][s]['Value'])
+        today = date.today()
+        temp2 = "{}.{}.{}".format(today.day, today.month, today.year)     
+        out =  str(f' На {temp2} курс {s} { temp1}')
         return out # Возвращение значения
     except Exception as e:
         return (f'Данных об этой валюте НЕТ' ) # Исключение, возможное при запросе
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Вводим наименование валюты и видим ее курс')
+    bot.send_message(m.chat.id, 'Вводим наименование валюты без учета регистра :')
 @bot.message_handler(content_types=["text"]) # Сообщение что будем искать
 def handle_text(message):
     global temp
