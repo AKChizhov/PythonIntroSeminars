3d8008167f4385efda548a5d0914eea3b55f4526
AKChizhov <ChizhovAK2007@yandex.ru>
2022-12-31 06:28:15 +0300

    +Доработано ДЗ к семинару №10

--- HomeWork№10/req.py
+++ HomeWork№10/req.py
@@ -3,10 +3,14 @@ from datetime import date
 
 #res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-
+dates = ['AUD' ,'AZN','GBR','AZN','AMD','BGN','EUR','JPY','USA','CAD','UAH']
 result = json.loads(res.text)
 today = date.today()
 temp2 = "{}.{}.{}".format(today.day, today.month, today.year)     
 print(f' На {temp2} курс валют :')  
 print(result['Valute']["USD"]['Name'], result['Valute']["USD"]['Value'])
 print(result['Valute']["EUR"]['Name'], result['Valute']["EUR"]['Value'])
+#print(len(result))
+#print(result.keys())
+print(result['Valute']['EUR']['NumCode'])
+print(list(result['valute']))
--- HomeWork№10/toSeminar10_02.py
+++ HomeWork№10/toSeminar10_02.py
@@ -3,26 +3,31 @@ from datetime import date
 bot = telebot.TeleBot(config.TOKEN)
 
 os.system('CLS')
+dates = 'AUD ,AZN,GBR,AZN,AMD,BGN,EUR,JPY,USD,CAD,UAH'
+
 def find(s):
     try:
         res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         result = json.loads(res.text)  
         s = s.upper()
         temp1 = str (result['Valute'][s]['Value'])
+        temp3 = str (result['Valute'][s]['Name'])
         today = date.today()
         temp2 = "{}.{}.{}".format(today.day, today.month, today.year)     
-        out =  str(f' На {temp2} курс {s} { temp1}')
+        out =  str(f' На {temp2} курс {s} ({temp3}) { temp1}')
         return out # Возвращение значения
     except Exception as e:
         return (f'Данных об этой валюте НЕТ' ) # Исключение, возможное при запросе
 @bot.message_handler(commands=["start"])
+
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Вводим наименование валюты без учета регистра :')
+    bot.send_message(m.chat.id, f'Вводим наименование валюты без учета регистра :') 
+    bot.send_message(m.chat.id, f'Например : {dates}')    
 @bot.message_handler(content_types=["text"]) # Сообщение что будем искать
 def handle_text(message):
     global temp
     bot.send_message(message.chat.id, find(message.text))
     temp = message.text
-    print(f'Запрос на поиск курса {temp}')
+    print(f'Запрос на поиск курса {temp}  Программа активна')
 bot.polling(none_stop=True, interval=0)
 os.system('CLS')
